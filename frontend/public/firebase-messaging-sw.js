b469be851303bbb31f2ef89978ef2bae043ce7f9
6804062611220-Chalongrat <61728908+kimpk63@users.noreply.github.com>
2026-03-10 18:04:19 +0700

    vapidKey

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -2,10 +2,10 @@ importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCemKofa1yFw7eBqYNJp9VsLw-M_yiowQ1",
+  apiKey: "AIzaSyCenKofa1vFw7eBqYNJp9Vslw-W_yiowQI",
   projectId: "statuspr-b065b",
-  messagingSenderId: "1065812835926",
-  appId: "1:1065812835926:web:ab97f72dd33c6ff7a7dbe7",
+  messagingSenderId: "1058128359206",
+  appId: "1:1058128359206:web:ab97f72dd33c6ff7a7dbe7",
 });
 
 const messaging = firebase.messaging();
@@ -14,6 +14,6 @@ messaging.onBackgroundMessage((payload) => {
   console.log('[Service Worker] Background message:', payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: payload.notification.icon,
+    icon: payload.notification.icon || '/icon-192x192.png',
   });
 });
